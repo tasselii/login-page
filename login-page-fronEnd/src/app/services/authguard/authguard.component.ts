c77ae50b0ad92bd6e33203ae6f5174b9cf4b57ef
tasselii <thiagotasseli@outlook.com.br>
2024-08-27 16:23:45 -0300

    update

---
+++ login-page-fronEnd/src/app/services/authguard/authguard.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-authguard',
+  standalone: true,
+  imports: [],
+  templateUrl: './authguard.component.html',
+  styleUrl: './authguard.component.scss'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const authToken = sessionStorage.getItem('auth-token');
+
+    if (authToken) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
