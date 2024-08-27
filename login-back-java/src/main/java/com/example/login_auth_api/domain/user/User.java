46fad6c3972303485f46663c760e517edf337395
tasselii <thiagotasseli@outlook.com.br>
2024-08-27 15:23:19 -0300

    feat: upload back-end project to GitHub

---
+++ login-back-java/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -0,0 +1,52 @@
+package com.example.login_auth_api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ login-back-java/src/test/java/com/example/login_auth_api/controllers/AuthControllerTest.java
@@ -0,0 +1,7 @@
+package com.example.login_auth_api.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+}
